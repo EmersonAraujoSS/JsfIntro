be54fac6403b07ca65e42a60e813fe3f0aff8255
emerson.araujo <mcemersonaraujo@gmail.com>
2024-06-27 16:50:51 -0400

    Commit do Usuario Pessoa Intro
    Essa classe vai criar minha tabela no banco de dados

---
+++ src/main/java/br/com/educandoweb/model/UsuarioPessoaIntro.java
@@ -0,0 +1,83 @@
+package br.com.educandoweb.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+//MInha classe tem que estar com a anotação "@Entity" para informar que será uma tabela do banco de dados
+@Entity
+public class UsuarioPessoaIntro {
+
+
+    @Id //para gerar sempre com o numero id
+    @GeneratedValue(strategy = GenerationType.AUTO) //para não dar conflito de insert repetido
+    private Long id;
+
+    //Atributos
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String login;
+    private String senha;
+    private int idade;
+
+
+    //Metodos especiais
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "UsuarioPessoaIntro{" +
+                "id=" + id +
+                ", nome='" + nome
+                + ", sobrenome='" + sobrenome
+                + ", email='" + email
+                + ", login='" + login
+                + ", senha='" + senha
+                + ", idade=" + idade
+                + '}';
+    }
+}
