a121649cf3bc4e664038077c5fde8837fc8716a1
emerson.araujo <mcemersonaraujo@gmail.com>
2024-06-27 16:52:16 -0400

    Commit do Usuario Generic Dao
    Essa classe vai fazer ter os métodos de salvar, deleter e etc no banco de dados

---
+++ src/main/java/br/com/educandoweb/dao/GenericDao.java
@@ -0,0 +1,61 @@
+package br.com.educandoweb.dao;
+
+import br.com.educandoweb.HibernateUtil;
+import javax.persistence.EntityTransaction;
+import javax.persistence.EntityManager;
+
+
+public class GenericDao <E>{
+
+
+    private EntityManager entityManager = HibernateUtil.getEntityManager(); //aqui estou estanciando minha conexão com o banco de dados e passsando o metodo que faz update
+
+
+    //Metodo
+    public void salvar(E entity){
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(entity);
+        transaction.commit();
+
+    }
+
+    //Metodo
+    public E updateMerge(E entity){  //método que salva e atualiza no banco de dados
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        E entitySalva = entityManager.merge(entity);
+        transaction.commit();
+
+        return entitySalva;
+    }
+
+
+
+    public E pesquisar(E entity){
+
+        Object id = HibernateUtil.getPrimaryKey(entity);
+
+        E e = (E) entityManager.find(entity.getClass(), id);
+
+        return e;
+    }
+
+
+    public void deletePorId(E entity){
+
+        Object id = HibernateUtil.getPrimaryKey(entity);
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.createNativeQuery("delete from "+entity.getClass().getSimpleName().toLowerCase()+" where id="+id).executeUpdate();
+        transaction.commit();
+    }
+
+
+    public E pesquisar(Long id, Class<E> entity){
+        E e = (E) entityManager.find(entity, id);
+
+        return e;
+    }
+}
